54e2569224addec8b31f37a05b869252094dccb7
Dhareddy <37417779+Dhareddy@users.noreply.github.com>
2018-03-15 14:29:02 -0500

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,19 @@
+package string;
+
+	public class Reverse {
+
+	    public static void main(String[] args) {
+	        // TODO Auto-generated method stub
+	        String name="java";
+	        char value[]=name.toCharArray();
+	        for(int i=value.length-1;i>=0;i--)
+	        {
+	        System.out.print(value[i]);
+	        }
+	   
+	    }
+
+	}
+
+
+
